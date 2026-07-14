41e7dfb45953b57cdcda2d5123062176d79d540b
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-14 14:33:25 -0300

    feat: adiciona nova funcionalida de arquivo produto ts

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { UpperCasePipe, CurrencyPipe } from '@angular/common';
+import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
-  produto = 'notebook';
-  preco = 5000;
-  mostrarPreco = true;
-  mostrarProduto = true;
+ // produto = 'notebook';
+  //preco = 5000;
+  //mostrarPreco = true;
+  //mostrarProduto = true;
+  produtos = [
+    {produto: 'Monitor', preco: 1000},
+    {produto: 'Teclado', preco: 49.99},
+    {produto: 'Mause', preco: 25.50}
+  ];
 }
