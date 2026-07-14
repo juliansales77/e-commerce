a27bcc00c06fd7e641aa324e256f7b2f901645eb
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-14 17:17:22 -0300

    faet: cria components  lista-produto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 //!import { RouterOutlet } from '@angular/router'; // remover impotação  routeroutlet ,pois não necessário para component
 import {  Produto } from './components/produto/produto';
+import { ListaProdutos } from './components/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
-  imports: [ Produto],
+  imports: [ ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input,Output } from '@angular/core';
 import { UpperCasePipe, CurrencyPipe } from '@angular/common';
 import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
 
@@ -9,13 +9,6 @@ import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
   styleUrl: './produto.css',
 })
 export class Produto {
- // produto = 'notebook';
-  //preco = 5000;
-  //mostrarPreco = true;
-  //mostrarProduto = true;
-  produtos = [
-    {produto: 'Monitor', preco: 1000},
-    {produto: 'Teclado', preco: 49.99},
-    {produto: 'Mause', preco: 25.50}
-  ];
+ @Input() nome: string = '';
+ @Input() preco: number = 0;
 }
