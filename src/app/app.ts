623d3a6b53533dd22c9692b797ece1c746bad0d7
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-16 13:46:17 -0300

    feat:(src/app/app.ts) atualiza o arquivo app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 //!import { RouterOutlet } from '@angular/router'; // remover impotação  routeroutlet ,pois não necessário para component
-import {  Produto } from './components/produto/produto';
-import { ListaProdutos } from './components/lista-produtos/lista-produtos';
+import {  Produto } from './features/produtos/produto/produto';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
