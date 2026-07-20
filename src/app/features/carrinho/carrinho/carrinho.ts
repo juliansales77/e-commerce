545a6e66024b08a17f596fed12b40a914abbe042
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-20 16:43:58 -0300

    feat(features) atualiza o arquivo carrinho.css

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //lógica do carrinho virá em aulas futuras
+  //por ora, apenas exibi a página de carrinho
+}
