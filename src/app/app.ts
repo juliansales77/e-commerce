45f55e9d3343ada2e52e33dfd88b43f43abb727c
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-20 16:40:38 -0300

    atualiza o arquivo app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
-import { Component, signal } from '@angular/core';
-//!import { RouterOutlet } from '@angular/router'; // remover impotação  routeroutlet ,pois não necessário para component
+import { Component, signal } from '@angular/core'
 import {  Produto } from './features/produtos/produto/produto';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+//import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet,RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ ListaProdutos],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'CompraFácil';
 }
