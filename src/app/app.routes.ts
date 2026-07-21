78106e058e07c708fe1738ca5a230878bb8c4b2a
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-07-21 17:01:08 -0300

    feat: atualiza o arquivo app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,49 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
+//import { Routes } from '@angular/router';
+//import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+//import { Carrinho } from './features/carrinho/carrinho/carrinho';
+//import { Home } from './features/home/home/home';
 
+//export const routes: Routes = [
+
+   // {
+       // path: '',
+        //component: Home,
+    //},
+    //código legado com lazyLoading
+   // {
+   //     path: 'produtos',
+  //      component: ListaProdutos,
+  //  },
+    //{
+     //   path: 'produtos',
+       // loadComponent: () =>
+         //   import ('./features/produtos/lista-produtos/lista-produtos').then(m =>m.ListaProdutos)
+    //},
+    //{
+     //   path: 'carrinho',
+      //  component: Carrinho,
+    //},
+//];
+// ! código final
+import { Routes} from "@angular/router";
 export const routes: Routes = [
     {
-        path: '',
-        component: ListaProdutos
+path: '',
+loadComponent: () =>
+    import ('./features/home/home/home').then((m) => m.Home),
+    },
+    {
+        path: 'produtos',
+        loadComponent: () => 
+            import ('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos)
     },
     {
         path: 'carrinho',
-        component: Carrinho,
+        loadComponent: () =>
+        import ('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
+    },
+    {
+       path: '**',
+       redirectTo: '',
     },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,5 @@
 import { Component, signal } from '@angular/core'
-import {  Produto } from './features/produtos/produto/produto';
-//import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { RouterOutlet,RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
