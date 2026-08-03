d8291518ca066b4ba76b73829c30ad0b990c9f42
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-03 16:08:34 -0300

    echo crie uma nova pasta interceptores

---
+++ src/app/core/interceptors/http.interceptores.ts
@@ -0,0 +1,37 @@
+import { HttpInterceptorFn} from "@angular/common/http";
+import { Token } from "@angular/compiler";
+import { error } from "console";
+import { tap } from "rxjs";
+import { catchError } from "rxjs";
+import { throwError } from "rxjs";
+
+export const httpInterceptor: HttpInterceptorFn = (req, next) => {
+
+//! token
+    const token = 'fake-jwt-token';
+
+    const novaReq = req.clone({
+        setHeaders: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    console.log('Interceptando requisição: ', req.url);
+    return next(novaReq).pipe(
+        tap({
+            next: (event) => console.log('RESPONDE: ', event ),
+            error: (error) => console.log('ERRO: ', error)
+        }),
+        catchError((error) => {
+
+            console.error('ERRO GLOBAL:', error);
+            
+            if (error.status === 401){
+                console.warn('Não Autorizado!');
+            }
+           if (error.status === 500) {
+            console.warn('Erro Interno do Servidor!')!;
+           }
+           return throwError(() => error);
+        }),
+    );
+};
