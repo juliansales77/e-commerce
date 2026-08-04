3b3a180ead2c6a1969c3d685fc1bc55d521a5bec
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-04 17:07:20 -0300

    faet: atualiza o arquivo app.html

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router'; 
+
 
 @Component({
   selector: 'app-header',
