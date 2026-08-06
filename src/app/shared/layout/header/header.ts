41ed3f51d193cfd1e40160e8489ef312f1587ca1
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-06 15:16:00 -0300

    feat: adiciona acesso à quantidade de itens do carrinho

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -3,15 +3,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router'; 
-
+import { inject } from '@angular/core';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   
 loja = 'comprafácil';
+private  CarrinhoService = inject (CarrinhoService)
+quantidade = this.CarrinhoService.quantidadeItens;
 }
