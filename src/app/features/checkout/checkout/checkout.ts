9bf53597da9cc8d81d42a872a53f7d717679b41e
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-06 16:42:42 -0300

    feat(features): cria estrutura inicial do módulo de checkout

---
+++ src/app/features/checkout/checkout/checkout.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { email } from '@angular/forms/signals';
+
+@Component({
+  selector: 'app-checkout',
+  imports: [ReactiveFormsModule],
+  templateUrl: './checkout.html',
+  styleUrl: './checkout.css',
+})
+export class Checkout {
+  carrinhoService = inject(CarrinhoService);
+
+  formulario = new FormGroup({
+    nome: new FormControl(''),
+    email: new FormControl(''),
+    endereco: new FormControl(''),
+  });
+  finalizar (){
+    console.log('Dados do Formulario: ', this.formulario.value);
+    console.log('Itens do Carrinho: ', this.carrinhoService.itens())
+  }
+}
