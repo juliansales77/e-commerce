02ba7f4d2fe7b9d6d96d768b1297a3efe0a2fd1e
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-10 15:58:20 -0300

    feat(carrinho): adiciona tipo ItemCarrinho

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+}
+
 @Injectable({
     providedIn: 'root'
 })
 export class CarrinhoService {
     //! estado global
-    private carrinho = signal<{nome: string; preco: number}[]>([]);
+    private carrinho = signal<ItemCarrinho[]>([]);
     
     itens = computed(() => this.carrinho());
     quantidadeItens = computed(() => this.carrinho(). length);
@@ -15,7 +20,9 @@ export class CarrinhoService {
        this.carrinho().reduce((total, item) => total + item.preco,0)
 );
 
-adicionar(produto: {nome: string; preco: number}){
+carrinhoVazio = computed(() => this.carrinho().length === 0);
+
+adicionar(produto: ItemCarrinho){
     this.carrinho.update(lista => [ ...lista, produto]);
 }
 limpar() {
