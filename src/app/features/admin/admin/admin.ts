6c3cd4a948ebaf5f7136221e603246e6882af988
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-14 15:08:37 -0300

    fix(admin): corrige lógica do painel administrativo

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,4 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  //!Simulação
+totalProdutosCadastrados = signal(20);
+pedidosPendentes = signal(3);
+usuariosCadastrados = signal (8);
+
+usuarioAtual = this.authService.usuarioAtual;
+
+mansagemPerfil = computed(() =>{
+  const usuario = this.usuarioAtual();
+
+  if (!usuario){
+    return('Nenhum usuario Autenticado!');
+  }
+  return `Usuario autenticado como: ${usuario.perfil}`;
+});
+
+sair(){
+  this.authService.logout();
+  this.router.navigateByUrl('/login');
+  }
+}
