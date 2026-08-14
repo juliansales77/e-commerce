eef15d864d3fbf5c68556c36abce50ef69338e41
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-14 15:15:32 -0300

    fix(header): corrige exibição do nome da loja no header.ts

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,33 +1,34 @@
-import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router'; 
-import { inject } from '@angular/core';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
-import { UpperCasePipe } from '@angular/common';
 import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink, UpperCasePipe],
+  imports: [MatButtonModule, MatToolbarModule, RouterLink, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-estaLogado() {
-throw new Error('Method not implemented.');
-}  
-loja = 'Full stack Store';
-private  CarrinhoService = inject (CarrinhoService)
-quantidade = this.CarrinhoService.quantidadeItens;
-private authService = inject(AuthService)
-usuarioLogado = this.authService.usuarioLogado;
-usuarioAtual = this.authService.usuarioAtual ;
-  router: any;
-
-sair() {
-this.authService.logout();
-this.router.navigateByUrl('/login');
-}
+  nomeLoja = 'Full Stack Store';
+  
+  private carrinhoService = inject(CarrinhoService);
+  quantidade = this.carrinhoService.quantidadeItens;
+  
+  private authService = inject(AuthService)
+  usuarioLogado = this.authService.usuarioLogado;
+  usuarioAtual = this.authService.usuarioAtual;
+  
+  private router = inject(Router)
+  
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login')
+  }
 }
