b93df622fb2fdef512431c7a4628995239c606ff
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-14 14:58:49 -0300

    refactor(usuario): adiciona tipagem para usuário e perfil

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,53 +1,53 @@
-import { Injectable } from "@angular/core";
-import { signal } from "@angular/core";
-import { computed } from "@angular/core";
-import { email } from "@angular/forms/signals";
+import { Injectable, computed, signal } from "@angular/core";
+
+type PerfilUsuario = 'admin' | 'usuario'; 
 
 type Usuario = {
     email: string;
-    perfil: 'usuario';
+    perfil: PerfilUsuario;
 }
+
 @Injectable({
     providedIn: 'root'
 })
 
 export class AuthService {
-    ehAdmin() {
-      throw new Error('Method not implemented.');
-    }
-    estaLogado() {
-        throw new Error("Method not implemented.");
-    }
-    private usuario = signal<Usuario | null>(null);
-    private tokenJwt = signal<string | null>(null);
 
-    usuarioAtual = computed(() => this.usuario());
+
+    private usuario = signal<Usuario | null> (null);
+    private tokenJwt = signal<string | null> (null);
+
+
     usuarioLogado = computed(() => this.usuario() !== null);
-    token = computed (() => this.tokenJwt());
+    usuarioAtual = computed(() => this.usuario());
+    estaLogado = computed(() => this.usuario() !== null);
+    token = computed(() => this.tokenJwt());
+    admin = computed(() => this.usuario()?.perfil ==='admin');
 
-    login(email: string, senha: string):boolean{
+    login (email: string, senha: string): boolean{
         if(!email || !senha){
             return false;
         }
-const tokenSimulado = 
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
-'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
-+
-'assinatura-simulada';
-
-this.usuario.set({
-    email,
-    perfil: 'usuario',
-});
-this.tokenJwt.set(tokenSimulado);
-return true;
-    }
-
-    logout(){
-        this.usuario.set(null);
-        this.tokenJwt.set(null);
-    }
+        const perfil: PerfilUsuario = email === 'admin@email.com' ? 'admin' : 'usuario'
+        const tokenSimulado = 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
+    'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
+    +
+    'assinatura-simulada';
+    
+    this.usuario.set({
+        email, 
+        perfil,
+    });
+    this.tokenJwt.set(tokenSimulado);
+    return true;
+}
+logout(){
+    this.usuario.set(null);
+    this.tokenJwt.set(null);
+}
     obterToken(): string | null{
         return this.tokenJwt();
     }
+
 }
