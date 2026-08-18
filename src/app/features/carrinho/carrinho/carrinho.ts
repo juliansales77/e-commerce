a448e58740ca0ec8baa47855cd864167206be514
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-18 17:19:38 -0300

    feat(carrinho): integra componente com facades e ações do carrinho

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthFacade } from '../../../core/facades/auth.facade';
+import { PrecoFormatadoPipe } from "../../../shared/pipes/preco-formatado-pipe";
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, PrecoFormatadoPipe],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
 export class Carrinho {
-  //lógica do carrinho virá em aulas futuras
-  //por ora, apenas exibi a página de carrinho
+
+  public carrinhoFacade = inject(CarrinhoFacade);
+  private router = inject(Router);
+  private authFacade = inject(AuthFacade);
+  
+  removerItem(rmvItem:number){
+    this.carrinhoFacade.limpaCarrinho();
+  }
+  limpaCarrinho(){
+    this.carrinhoFacade.limpaCarrinho();
+  }
+  cancelarCompra(){
+    this.authFacade.sair();
+    this.router.navigateByUrl('/login')
+  }
+  
 }
