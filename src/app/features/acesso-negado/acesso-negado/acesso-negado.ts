2615482229ad6ba09612872831037bd71e45a351
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-18 17:16:39 -0300

    feat(acesso-negado): adiciona logout e redirecionamento para login

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthFacade } from '../../../core/facades/auth.facade';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-acesso-negado',
-  imports: [RouterLink],
+  imports: [RouterLink, MatAnchor],
   templateUrl: './acesso-negado.html',
   styleUrl: './acesso-negado.css',
 })
-export class AcessoNegado {}
+export class AcessoNegado {
+  private authFacade = inject(AuthFacade); //! teste em produção
+  private router = inject(Router)
+
+
+
+  sair(){
+    this.authFacade.sair();
+    this.router.navigateByUrl('/Login');
+    return;
+  } 
+}
