05f86e87b7c6d3acaee6c397869f680e6a96075e
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-18 14:28:08 -0300

    refactor(auth): utiliza AuthFacade no admin guard

--- src/app/core/admin.guard.ts
+++ src/app/core/admin.guard.ts
@@ -1,19 +1,19 @@
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { CanActivateFn } from "@angular/router";
-import { AuthService } from "./services/auth.service";
+import { AuthFacade } from "./facades/auth.facade";
 
 export const adimnGuard: CanActivateFn = () => {
 
     const router = inject(Router);
-    const authService = inject(AuthService)
+    const authFacade = inject(AuthFacade)
 
 //! - 1) Verifecar se o usuario esta logado
-if(!authService.usuarioLogado()){
+if(!authFacade.usuarioLogado()){
     return router.createUrlTree(['/acesso-negado']);
     }
     //! - 2) Verificar se o usuario atual (Logado), se tem perfil adm
-    if(!authService.admin()){
+    if(!authFacade.admin()){
         return router.createUrlTree(['/acesso-negado'])
     }
     //! - 3) se o usuario estiver logado e for adm = ACESSO LIBERADO
