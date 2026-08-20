2980a71a7d4ed9935fb1ef47c32cf39298b0eeda
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-20 13:55:13 -0300

    feat(produto): adiciona eventos de seleção e inclusão no carrinho

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -3,6 +3,7 @@ import { UpperCasePipe, CurrencyPipe } from '@angular/common';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule, MatCard } from '@angular/material/card';
+import { ItemCarrinho } from '../../../core/models/item-carrinho';
 
 @Component({
   selector: 'app-produto',
@@ -21,10 +22,7 @@ export class Produto {
 selecionadoProduto()  {
   this.produtoSelecionado.emit(this.nome);
 }
-@Output() produtoAdicionado = new EventEmitter<{
-  nome: string;
-  preco: number;
-}>();
+@Output() produtoAdicionado = new EventEmitter<ItemCarrinho>();
 
 adicionarAoCarrinho() {
   this.produtoAdicionado.emit({nome: this.nome, preco: this.preco});
