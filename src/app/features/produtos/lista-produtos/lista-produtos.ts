2321dc07ba4858b15667858a6d7608fcaa4fa20c
katherine moraes de paula <katherinemoraesdepaula@gmail.com>
2026-08-21 14:10:34 -0300

    feat(produtos): adiciona listagem, estado de carregamento e integração com carrinho

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,104 +1,118 @@
-import { Component, inject, } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { signal } from '@angular/core';
 import { Produto } from '../produto/produto';
 import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { effect } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
-import { error } from 'console';
+
 import { produtoService } from '../../../core/services/produtos.service';
-import { Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
-import { AuthFacade } from '../../../core/facades/auth.facade';
+import {MatCardModule} from '@angular/material/card';
+
+import { RouterLink } from '@angular/router';
+import { ProdutoLoja } from '../../../core/models/produto-loja';
 import { ItemCarrinho } from '../../../core/models/item-carrinho';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe, MatButtonModule, MatCardModule],
-  templateUrl: '../lista-produtos/lista-produtos.html',
+  imports: [ Produto, PrecoFormatadoPipe, UpperCasePipe, MatButtonModule, MatCardModule, RouterLink],
+  templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
+
 export class ListaProdutos {
+  //! remove a lista de produtos, dados carregados via API Fakestar 
+produtos = signal < ProdutoLoja[] > ([])
+carregando = signal(true);
+erro = signal < string | null > (null)
 
-produtos = signal <
-{nome: string; preco: number } []> ([]);
 
-  carregando = signal(true);
-  //! cria o método para requisição dos produto
-  carregarProduto(){
-   this.carregando.set(true)
-   this.erro.set(null);
+//? ============ COMPUTED ===============
 
-   this.produtosService.buscarProdutos().subscribe({
-    next: (dados) => {
-      const produtos = this.produtosService.transformarProdutos(dados);
-      this.produtos.set(produtos);
-      this.carregando.set(false);
-    },
-error: (erro) => {
-  console.error('erro ao carregar os  produtos:, ', erro);
-  this.erro.set('Erro ao carregar os Produtos. verifique sua conexão e tente novamente')
-  this.carregando.set(false);
-      },
-   });
-  }
+carregarProdutos(){
+  this.carregando.set(true);
+  this.erro.set(null), //? Limpa erro anterior
 
-  exibirProduto (nome: string){
-    console.log ('Produto selecionado: ', nome); 
-    this.produtoSelecionado.set(nome);
-  }
-  adicionarProduto(){
-    this.produtos.update(listaAtual => [
-      ...listaAtual, {nome: 'Processador core i5 15550FS', preco: 2500}
-    ]);
-  }
-   totalProdutos = computed(() => this.produtos().length); 
+  this.produtoService.buscarProdutos().subscribe({
+              next: (dados) => {
+                const produtos = this.produtoService.transformarProdutos(dados);
+                this.produtos.set(produtos);
+                this.carregando.set(false);
+              },
+              error: (erro) => {
+                console.error('Erro ao carregar os Produtos:, ', erro);
+                this.erro.set('Erro ao carregar Produtos. Verifique sua conexão e tente novamente.');
+                this.carregando.set(false);
+              },
 
-   valorTotal = computed (()=> {return this.produtos().reduce((total, item) => total + item.preco,0)});
 
-   substituirProdutos() {
-    this.produtos.set([
-      {nome: 'Teclado', preco: 40},
-      {nome: 'Mouse', preco: 10},
-      {nome: 'Monitor', preco: 100},
-      {nome: 'Desktop', preco : 500},
-      {nome: 'Hesdset', preco: 25},
-    ]);
-   }
-   constructor(){
-    this.carregarProduto();
-
-    effect(() => {
-      console.log('Lista de Produtos Alterados: ', this.produtos());
-    });
-    effect(() => {
-      console.log('Valor total atualizado: ', this.valorTotal());
-    });
-    effect(() => {
-      if (typeof document !== 'undefined') {
-        document.title = `(${this.totalProdutos()}) Minha Loja`;
-      }
-    });
-   }
-   produtoSelecionado = signal <string | null> (null);
-
-  carrinho = signal <{nome: string; preco: number}[]> ([]);
-
-  erro = signal <string | null> (null);
+  });
 
   
-   adicionarAoCarrinho (produto: ItemCarrinho){
-    this.carrinhoFacade.adicionarProdutoCarrinho(produto);
-   }
+}
 
-private produtosService = inject(produtoService);
-public carrinhoFacade = inject(CarrinhoFacade);
+exibirProduto (nome:string){
+   //console.log ('Produto selecionado: ', nome);
+   this.produtoSelecionado.set(nome);
+}
+adicionarProduto(){
+ this.produtos.update(listaAtual => [
+   ...listaAtual, { nome: 'Processador Core i5 14550FS', preco: 2500 }
+ ]);
+
+}
+totalProdutos = computed(() => this.produtos().length);
+
+valorTotal = computed(() => { 
+ return this.produtos().reduce
+ ((total, item) => total + item.preco, 0);
+});
+
+valorTotalFormatado = computed(()=> this.valorTotal().toFixed(2));
+
+subtituiProdutos(){
+   this.produtos.set([
+      { nome: 'Teclado', preco: 40 },
+        { nome:'Mouse', preco: 10 },
+         { nome:'Monitor', preco:100 },
+          { nome:'Desktop', preco:500 },
+           { nome:'Headset', preco:25 },
+   ]);
+
+}
+//! injetar httpClient dentro de construct, reestruturar construtor
+//? metodo http (API)
+
+constructor( ){
+  //! carregar api
+  this.carregarProdutos();
+  
+
+  //! effect se mantém o mesmo
+ 
+
+ effect(() => {
+if (typeof document !== 'undefined') {
+    document.title = `(${this.totalProdutos()}) Minha Loja`
+}
+ });
+
+ }
+ produtoSelecionado = signal<string | null> (null);
+ 
+
+ adicionarAoCarrinho(produto: ItemCarrinho){
+  this.carrinhoFacade.adicionarProdutoCarrinho(produto);
+ }
+ 
+
+//? ============ INJECT ==============
+private produtoService = inject(produtoService); //lembrete: Nossa variável é produtoService e Não produtosService
+carrinhoFacade = inject(CarrinhoFacade);
 
 quantidadeCarrinho = this.carrinhoFacade.quantidadeCarrinho;
 totalCarrinho = this.carrinhoFacade.totalCarrinho;
-
 }
-
-  
